959b4794592ebc19859433843c08d26c62c0a675
Sanmoy Adhikari <95347262+neel-adhikari@users.noreply.github.com>
2026-03-13 12:57:58 +0530

    Refactor typing effect code in script.js

--- script.js
+++ script.js
@@ -2,31 +2,32 @@ const text = [
 "MBA Student",
 "Business Analytics Enthusiast",
 "Automation Builder"
-];
+]
 
-let count = 0;
-let index = 0;
-let currentText = "";
-let letter = "";
+let count = 0
+let index = 0
+let currentText = ""
+let letter = ""
 
 function type(){
 
 if(count === text.length){
-count = 0;
+count = 0
 }
 
-currentText = text[count];
-letter = currentText.slice(0, ++index);
+currentText = text[count]
 
-document.querySelector(".typing").textContent = letter;
+letter = currentText.slice(0, ++index)
+
+document.querySelector(".typing").textContent = letter
 
 if(letter.length === currentText.length){
-count++;
-index = 0;
+count++
+index = 0
 }
 
-setTimeout(type,120);
+setTimeout(type,120)
 
 }
 
-type();
+type()
